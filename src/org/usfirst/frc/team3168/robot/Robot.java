c3ec0b62a91d0d0b5082ba2f51646f6899e154fb
Team3168 <dawnrountree@spsk12.net>
2017-01-21 14:33:09 -0500

    Removed usless struts

--- src/org/usfirst/frc/team3168/robot/Robot.java
+++ src/org/usfirst/frc/team3168/robot/Robot.java
@@ -50,23 +50,6 @@ public class Robot extends IterativeRobot {
 			
 		}
 		
-		
-		
-	
-	
-	public static void robotMotors() {
-		
-		
-	}
-	
-	public void robotJoysticks() {
-
-	}
-	
-	public static void robotXBOX() {
-		
-	}
-
 	/**
 	 * This autonomous (along with the chooser code above) shows how to select
 	 * between different autonomous modes using the dashboard. The sendable
